801d01e6b96c503df9c98d39c955ebc46b98f9d7
MaksimMerkushev <m.merkushev@innopolis.university>
2026-02-23 15:27:08 +0300

    feat: implement GET /learners endpoint

--- src/app/routers/learners.py
+++ src/app/routers/learners.py
@@ -2,14 +2,14 @@
 
 from fastapi import APIRouter
 
-# from datetime import datetime
-#
-# from fastapi import Depends
-# from sqlmodel.ext.asyncio.session import AsyncSession
-#
-# from app.database import get_session
-# from app.db.learners import read_learners, create_learner
-# from app.models.learner import Learner, LearnerCreate
+from datetime import datetime
+
+from fastapi import Depends
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.database import get_session
+from app.db.learners import read_learners, create_learner
+from app.models.learner import Learner, LearnerCreate
 
 router = APIRouter()
 
@@ -40,12 +40,12 @@ async def get_learners(
 # ===
 
 # UNCOMMENT AND FILL IN
-@router.post("/", status_code=201, response_model=Learner)
-async def post_learner(
-    learner: LearnerCreate,
-    session: AsyncSession = Depends(get_session),
-) -> Learner:
-    return await create_learner(session=session, name=learner.name, email=learner.email)
+#@router.post("/", status_code=201, response_model=Learner)
+#async def post_learner(
+#    learner: LearnerCreate,
+#    session: AsyncSession = Depends(get_session),
+#) -> Learner:
+#    return await create_learner(session=session, name=learner.name, email=learner.email)
 # @router.<method>("/<resource_name>", response_model=<resource_schema>, status_code=<status_code>)
 # async def <function_name>(
 #     <param_name>: <request_schema>,
